c50a412534d105ffbfbb242f78c446ccfa370bcb
Erick Antelo <erickantelo@hotmail.com>
2024-06-29 23:31:35 -0400

    contacto page

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask
+import datetime
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Erick el Mejor!'
+
+@app.route('/contacto')
+def hello_world():
+    # get today date and time
+    
+    now = datetime.datetime.now()
+    # return email and now date and time
+    return 'eri@hotmail.com' + now.strftime("%Y-%m-%d %H:%M:%S")
+    
